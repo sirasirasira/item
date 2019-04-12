3c43c62507183ba0c66f4dd4ce7a12e41ec64403
Ryo Shirakawa <ryo.shirakawa.recruit@gmail.com>
2019-04-12 12:31:42 +0900

    procon comment out ---end---

--- template/procon/main.cpp
+++ template/procon/main.cpp
@@ -11,6 +11,6 @@
 using namespace std;
 
 int main() {
-	std::cout << "\e[38;5;0m\e[48;5;40m --- end ---  \e[m" << std::endl; // debug
+	// std::cout << "\e[38;5;0m\e[48;5;40m --- end ---  \e[m" << std::endl; // debug
 	return 0;
 }
